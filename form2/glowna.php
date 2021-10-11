1b7cf7ad21f5b1c6eed2cf5e04b432b89921d228
MichalPlucinski1 <mic.plu@wp.pl>
2021-10-11 21:22:20 +0200

    poprawka

---
+++ form2/glowna.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Średni wiek</title>
+  </head>
+  <body>
+    <?php
+    if (!isset($_POST['person']) && !isset($_POST['srednia'])) {
+      echo <<<ETYKIETA
+      <h3>Ilość osób w rodzinie</h3>
+      <form method="post">
+        <input type="number" name="person" placeholder="Podaj ilość osób"><br><br>
+        <input type="submit" name="zatwierdź">
+
+      </form>
+      ETYKIETA;
+      }
+
+
+    if (!empty($_POST['person'])) {
+      echo "<h3>Ilość osób w rodzinie: $_POST[person]</h3>";
+      echo <<<ETYKIETA
+      <form method="post">
+      ETYKIETA;
+        for ($i=1; $i < $_POST['person'] + 1; $i++) {
+          echo <<<ETYKIETA
+          <input type="number" name="person" placeholder="wiek osoby nr=
+ETYKIETA;
+
+          echo "$i" ,"\">", "<br>";
+        }
+      echo <<<ETYKIETA
+        <input type="submit" name="srednia" value="zatwierdź">
+        </form>
+        ETYKIETA;
+
+
+if (isset($_POST['srednia'])) {
+
+
+  $avg = 0;
+  $count = 0;
+  foreach ($_POST as $key => $value) {
+    if ($key != 'srednia') {
+      $avg= $value;
+      $count++;
+    }
+  }
+echo "średni wiek = " , number_format($avg/$count,2)."lat<hr>";
+echo <<<ETYKIETA
+ "<a href="glowna.php">strona główna</a>";
+ ETYKIETA;
+}
+    }
+
+
+
+
+    ?>
+  </body>
+</html>
